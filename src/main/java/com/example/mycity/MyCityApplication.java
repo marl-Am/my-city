8ce182fa3369e207576221549d5ba19c458b8301
marl-Am <marl-Am@users.noreply.github.com>
2022-12-23 02:58:35 -0500

    Created project

---
+++ src/main/java/com/example/mycity/MyCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCityApplication.class, args);
+	}
+
+}
